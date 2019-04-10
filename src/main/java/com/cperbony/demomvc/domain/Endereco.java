cfdac8d246c23e3515883b3a80b86e038100c45d
Claudinei Perboni <cperbony@gmail.com>
2019-04-10 16:04:17 -0300

    Mapeamento entidades continuacao

---
+++ src/main/java/com/cperbony/demomvc/domain/Endereco.java
@@ -0,0 +1,85 @@
+package com.cperbony.demomvc.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ENDRECOS")
+public class Endereco extends AbstractEntity<Long> {
+
+    @Column(nullable = false)
+    private String logadouro;
+
+    @Column(nullable = false)
+    private String bairro;
+
+    @Column(nullable = false)
+    private String cidade;
+
+    @Column(nullable = false, length = 2)
+    @Enumerated(EnumType.STRING)
+    private UF uf;
+
+    @Column(nullable = false, length = 9)
+    private String cep;
+
+    @Column(nullable = false, length = 5)
+    private Integer numero;
+
+    private String complemento;
+
+    public String getLogadouro() {
+        return logadouro;
+    }
+
+    public void setLogadouro(String logadouro) {
+        this.logadouro = logadouro;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public UF getUf() {
+        return uf;
+    }
+
+    public void setUf(UF uf) {
+        this.uf = uf;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+}
