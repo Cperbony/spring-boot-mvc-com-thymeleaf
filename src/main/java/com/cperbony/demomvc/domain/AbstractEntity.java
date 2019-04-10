7f1a711487ca57216089cb790202836424b32155
Claudinei Perboni <cperbony@gmail.com>
2019-04-10 15:49:24 -0300

    Mapeamento entidades

---
+++ src/main/java/com/cperbony/demomvc/domain/AbstractEntity.java
@@ -0,0 +1,41 @@
+package com.cperbony.demomvc.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class AbstractEntity<ID extends Serializable> implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private ID id;
+
+    public ID getId() {
+        return id;
+    }
+
+    public void setId(ID id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity<?> that = (AbstractEntity<?>) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "AbstractEntity{" +
+                "id=" + id +
+                '}';
+    }
+}
